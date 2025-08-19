a8fe5b0e986162f2deae88c8356633b9187df7bb
Bd1boysss <saputrabin03@gmail.com>
2025-08-20 05:26:51 +0700

    Update route.ts

--- app/api/story/register/route.ts
+++ app/api/story/register/route.ts
@@ -1,15 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  return NextResponse.json({
+  return Response.json({
     ok: true,
     msg: "Stub register API jalan ✅ v3",
-  });
-}
-
-export async function GET() {
-  return NextResponse.json({
-    ok: true,
-    msg: "GET endpoint jalan juga 🚀",
   });
 }
